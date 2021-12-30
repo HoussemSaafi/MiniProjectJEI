514951e9b90d7c6a2fafcc189a4ed5745b3ae78e
HoussemSaafi <houssemsaafi@insat.u-carthage.tn>
2021-12-30 13:59:39 +0100

    NavBar component - Ismail work

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import logo from './logow.png'
+import '../style/NavBar.css'
+import profile from './pp.png'
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <img src={logo} alt='ollert logo'></img>
+            <h2>Ollert</h2>
+            <div className='pageDisp'>
+                <h3>Home</h3>
+            </div>
+            <div className="searchBar">
+                <input type="text" placeholder="Search for Something..."/>
+            </div>
+            <div className='pp'>
+            <img src={profile} alt='profile'></img>
+            </div>
+            
+
+        </nav>
+     );
+}
+ 
+export default Navbar;
